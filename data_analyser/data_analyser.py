24ce2165c6bfbce6b3fe69a6dceadd499c3baab3
cdne <cduneata@gmail.com>
2019-11-27 17:57:48 +0200

    Remove duplicate function

--- data_analyser/data_analyser.py
+++ data_analyser/data_analyser.py
@@ -16,27 +16,6 @@ from crm import crm
 import data_manager
 
 
-def choose_data_analyser(data_analyser_menu_list):
-    data_analyser_menu_active = True
-    while data_analyser_menu_active is True:
-        inputs = ui.get_inputs(["Please enter a number: "], "")
-        option = inputs[0]
-        if option == '1':
-            get_the_last_buyer_name()
-        elif option == '2':
-            get_the_last_buyer_id()
-        elif option == '3':
-            ui.print_result(get_the_buyer_name_spent_most_and_the_money_spent(
-            ), f'Customer with the most money spent: ')
-        elif option == '4':
-            get_the_buyer_id_spent_most_and_the_money_spent()
-        elif option == '5':
-            get_the_most_frequent_buyers_names(num=1)
-        elif option == '6':
-            get_the_most_frequent_buyers_ids(num=1)
-        elif option == '0':
-            data_analyser_menu_active = False
-
 
 def choose_data_analyser(data_analyser_menu_list):
     data_analyser_menu_active = True
@@ -44,30 +23,7 @@ def choose_data_analyser(data_analyser_menu_list):
         inputs = ui.get_inputs(["Please enter a number: "], "")
         option = inputs[0]
         if option == '1':
-            get_the_last_buyer_name()
-        elif option == '2':
-            get_the_last_buyer_id()
-        elif option == '3':
-            get_the_buyer_name_spent_most_and_the_money_spent()
-        elif option == '4':
-            get_the_buyer_id_spent_most_and_the_money_spent()
-        elif option == '5':
-            frequent_buyers_number = int(ui.get_inputs(['frequent buyers you want to see: '], 'Please input the number of top ')[0])
-            ui.print_result(get_the_most_frequent_buyers_names(frequent_buyers_number), 'Most frequent buyer(s) names, and number of sales: ')
-        elif option == '6':
-            frequent_buyers_number = int(ui.get_inputs(['frequent buyers you want to see: '], 'Please input the number of top ')[0])
-            ui.print_result(get_the_most_frequent_buyers_ids(frequent_buyers_number), 'Most frequent buyer(s) id(s), and number of sales: ')
-        elif option == '0':
-            data_analyser_menu_active = False
-
-
-def choose_data_analyser(data_analyser_menu_list):
-    data_analyser_menu_active = True
-    while data_analyser_menu_active is True:
-        inputs = ui.get_inputs(["Please enter a number: "], "")
-        option = inputs[0]
-        if option == '1':
-            get_the_last_buyer_name()
+            ui.print_result(get_the_last_buyer_name(), 'Last buyer\'s name ')
         elif option == '2':
             get_the_last_buyer_id()
         elif option == '3':
@@ -109,8 +65,9 @@ def get_the_last_buyer_name():
     Returns:
         str: Customer name of the last buyer
     """
+    table = data_manager.get_table_from_file('sales/sales.csv')
 
-    # your code
+
 
 
 def get_the_last_buyer_id():
